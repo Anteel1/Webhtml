7ed592f71e52f7062f27af1a313db342d65d8892
Anteel1 <100013057+Anteel1@users.noreply.github.com>
2022-07-17 01:10:33 +0700

    upload(2)

--- abcjs.js
+++ abcjs.js
@@ -4,13 +4,15 @@
         a.style.display ="none";
     });
     // run text 
-var text ="Chào mừng các bạn đã đến trang web";
+var text ="Chào các bạn đã ghé thăm";
 var i = 0;
 function typing(){
     if(i<text.length){
         document.getElementById("runtext").innerHTML+=text.charAt(i);
         i++;
-        setTimeout(typing,500); 
+        setTimeout(typing,300); 
+       
     }
 }
+
 typing();
